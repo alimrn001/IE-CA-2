aa270deee4c85b5529fee8f13d87e89c1b218c25
mehrani <mehraniali8002@gmail.com>
2023-03-11 13:29:16 +0330

    added http request handler
    tried sending a request in main, but server still refuses to connect.

--- IE-CA-2/src/main/java/com/mehrani/Main.java
+++ IE-CA-2/src/main/java/com/mehrani/Main.java
@@ -1,7 +1,18 @@
 package com.mehrani;
 
+import com.mehrani.HTTPReqHandler.HTTPReqHandler;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HTTPReqHandler reqHandler = new HTTPReqHandler();
+        String res = "";
+        try {
+            res = reqHandler.httpGetRequest("http://5.253.25.110:5000/api/users");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println(res);
     }
 }
